5a33bf16e220fd4312d48409dac1b06747a5bc2a
Israel Kabesa <israelkabesa@gmail.com>
2024-09-25 21:22:55 +0300

    Adding the Data as data into the database models

--- blog/views.py
+++ blog/views.py
@@ -1,43 +1,6 @@
-from datetime import date
-from django.utils.text import slugify
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-all_posts = [
-    {
-        "slug": "java",
-        "image": "about.png",
-        "author": "Israel Kabesa",
-        "date": date.today(),
-        "title": "Java Tools",
-        "excerpt": "There's nothing like the feeling of finishing your first Big Project, and creating new application",
-        "content": """in our BattleWord Application we used a lot of Java for generating the Automatic CrossWord 
-        Generator by Using Java language."""
-    },
-    {
-        "slug": "react",
-        "image": "BattleWord.png",
-        "author": "Israel Kabesa",
-        "date": date.today(),
-        "title": "TS and React Tools",
-        "excerpt": "There's nothing like the feeling of finishing your first Big Project, and creating new application",
-        "content": """in our BattleWord Application we used a lot of React and TypeScript for generating the
-         Frontend of our BattleWord project."""
-    },
-    {
-        "slug": "python",
-        "image": "GameMode.png",
-        "author": "Israel Kabesa",
-        "date": date.today(),
-        "title": "Python Tools",
-        "excerpt": "There's nothing like the feeling of finishing your first Big Project, and creating new application",
-        "content": """in our BattleWord Application we used a lot of Python for generating the BeckEnd of our BattleWord 
-        project, by Using Django framework."""
-    }
-]
-
-
-
-# Create your views here.
+from .models import Post
 
 
 def get_date(post):
@@ -45,21 +8,22 @@ def get_date(post):
 
 
 def home(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    id_post = next(post for post in all_posts if post["slug"] == slug)
+    id_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": id_post
+        "post": id_post,
+        "post_tags": id_post.tags.all(),
     })
